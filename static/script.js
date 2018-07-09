7cd1380ef00cbe23fca28ced209a6fc34734ff6e
Minichota <minichota1219@github.com>
2018-07-09 11:55:41 -0500

    Current Version

--- static/script.js
+++ static/script.js
@@ -1,5 +1,32 @@
-function wow(){
-$(document).ready(function(){
-        $("card1").remove();
-});
+console.log('JS loaded');
+function card1(){
+$(".card1").remove();
+}
+
+function card2(){
+$(".card2").remove();
+}
+
+function card3(){
+$(".card3").remove();
+}
+
+function card4(){
+$(".card4").remove();
+}
+
+function card5(){
+$(".card5").remove();
+}
+
+function card6(){
+$(".card6").remove();
+}
+
+function card7(){
+$(".card7").remove();
+}
+
+function card8(){
+$(".card8").remove();
 }
